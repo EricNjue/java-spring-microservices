b3cfb7851fc31293c7e5f7eab7b9049e3f0b83b8
Eric Gitonga Njue <eric.gitonga38@gmail.com>
2019-06-06 16:46:43 +0300

    Implementing Spring Cloud Sleuth

--- currency-conversion-service/src/main/java/com/devericnjue/microservices/currencyconversionservice/proxies/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/devericnjue/microservices/currencyconversionservice/proxies/CurrencyExchangeServiceProxy.java
@@ -6,11 +6,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "currency-exchange-service", url = "localhost:8000") :- required if ribbon is not being used
-@FeignClient(name = "currency-exchange-service")
+
+//@FeignClient(name = "currency-exchange-service") // this will communicate with currency-exchange-service without using the zuul gateway
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionResult getExchangeRate(@PathVariable("from") String from, @PathVariable("to") String to);
 }
--- netflix-eureka-naming-server/src/main/java/com/devericnjue/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ netflix-eureka-naming-server/src/main/java/com/devericnjue/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -1,8 +1,10 @@
 package com.devericnjue.microservices.netflixeurekanamingserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -13,4 +15,8 @@ public class NetflixEurekaNamingServerApplication {
         SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
